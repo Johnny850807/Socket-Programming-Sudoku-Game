bd25f26ade9f9c449de9fff694984344255ff373
johnny850807 <johnny850807@gmail.com>
2020-05-30 19:22:54 +0800

    clean

--- src/test/java/SudokuTest.java
+++ src/test/java/SudokuTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sudoku.Sudoku;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
